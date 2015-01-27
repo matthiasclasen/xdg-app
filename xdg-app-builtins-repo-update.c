6220dc1889eab09fca2479d25006db4efba87f1e
Matthias Clasen <mclasen@redhat.com>
2015-01-27 00:01:15 -0500

    repo-update: Create appstream data
    This calls out to appstream-builder to generate appstream data
    and icons for the applications in the repository, and commits
    the resulting files in a branch with name "appstream".

--- xdg-app-builtins-repo-update.c
+++ xdg-app-builtins-repo-update.c
@@ -11,6 +11,97 @@
 #include "xdg-app-utils.h"
 
 
+static gboolean
+ostree_repo_create_appstream (OstreeRepo *repo, GCancellable *cancellable, GError **error)
+{
+  gboolean ret = FALSE;
+  const char *location;
+  gs_free char *base = NULL;
+  gs_strfreev char **argv = NULL;
+  gs_free char *cmdline = NULL;
+  const char *subject;
+  const char *body;
+  const char *branch;
+  gs_free char *parent = NULL;
+  gs_free char *commit_checksum = NULL;
+  gs_unref_object OstreeMutableTree *mtree = NULL;
+  OstreeRepoTransactionStats stats;
+  gs_unref_object GFile *root = NULL;
+  gs_unref_object GFile *base_file = NULL;
+  gs_unref_object GFile *appstream_dir = NULL;
+
+  location = gs_file_get_path_cached (ostree_repo_get_path (repo));
+
+  base = g_dir_make_tmp ("xdg-appXXXXXX", error);
+  if (!base)
+    goto out;
+
+  base_file = g_file_new_for_path (base);
+
+  argv = g_new (char *, 11);
+  argv[0] = g_strdup ("appstream-builder");
+  argv[1] = g_strdup ("--enable-hidpi");
+  argv[2] = g_strdup ("--origin=GNOME");
+  argv[3] = g_strdup ("--api-version=0.8");
+  argv[4] = g_strdup ("--uncompressed-icons");
+  argv[5] = g_strconcat ("--ostree-repo=", location, NULL);
+  argv[6] = g_strconcat ("--log-dir=", base, "/logs", NULL);
+  argv[7] = g_strconcat ("--output-dir=", base, "/appstream", NULL);
+  argv[8] = g_strconcat ("--icons-dir=", base, "/appstream/icons", NULL);
+  argv[9] = g_strconcat ("--screenshot-dir=", base, "/screenshots", NULL);
+  argv[10] = NULL;
+
+  cmdline = g_strjoinv (" ", argv);
+  g_debug ("Running %s\n", cmdline);
+
+  if (!g_spawn_sync (NULL, argv, NULL, G_SPAWN_SEARCH_PATH | G_SPAWN_STDOUT_TO_DEV_NULL, NULL, NULL, NULL, NULL, NULL, error))
+    goto out;
+
+  subject = "Update appstream data";
+  body = "";
+  branch = "appstream";
+
+  if (!ostree_repo_resolve_rev (repo, branch, TRUE, &parent, error))
+    goto out;
+
+  if (!ostree_repo_prepare_transaction (repo, NULL, cancellable, error))
+    goto out;
+
+  appstream_dir = g_file_get_child (base_file, "appstream");
+
+  mtree = ostree_mutable_tree_new ();
+  if (!ostree_repo_write_directory_to_mtree (repo, appstream_dir, mtree, NULL, cancellable, error))
+    goto out;
+
+  if (!ostree_repo_write_mtree (repo, mtree, &root, cancellable, error))
+    goto out;
+
+  if (!ostree_repo_write_commit (repo, parent, subject, body, NULL,
+                                 OSTREE_REPO_FILE (root),
+                                 &commit_checksum, cancellable, error))
+    goto out;
+
+  ostree_repo_transaction_set_ref (repo, NULL, branch, commit_checksum);
+
+  if (!ostree_repo_commit_transaction (repo, &stats, cancellable, error))
+    goto out;
+
+  g_print ("Commit: %s\n", commit_checksum);
+  g_print ("Metadata Total: %u\n", stats.metadata_objects_total);
+  g_print ("Metadata Written: %u\n", stats.metadata_objects_written);
+  g_print ("Content Total: %u\n", stats.content_objects_total);
+  g_print ("Content Written: %u\n", stats.content_objects_written);
+  g_print ("Content Bytes Written: %" G_GUINT64_FORMAT "\n", stats.content_bytes_written);
+
+  ret = TRUE;
+
+out:
+  if (base)
+    gs_shutil_rm_rf (base_file, cancellable, NULL);
+
+  return ret;
+}
+
 gboolean
 xdg_app_builtin_repo_update (int argc, char **argv, GCancellable *cancellable, GError **error)
 {
@@ -42,7 +133,8 @@ xdg_app_builtin_repo_update (int argc, char **argv, GCancellable *cancellable, G
   if (!ostree_repo_regenerate_summary (repo, NULL, cancellable, error))
     goto out;
 
-  /* TODO: appstream data */
+  if (!ostree_repo_create_appstream (repo, cancellable, error))
+    goto out;
 
   ret = TRUE;
 
